33a9a4857e808ca3cbf7c094eaef4194ad64e236
wellingtonsilva04 <wel.dss@gmail.com>
2023-07-24 13:44:37 -0300

    conf. inicial, migrate  database, register, login

---
+++ app/routes.py
@@ -0,0 +1,51 @@
+
+from flask import flash, redirect, render_template, url_for
+from flask_login import current_user, login_required, login_user, logout_user
+
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    user = {'username': 'wellington'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
